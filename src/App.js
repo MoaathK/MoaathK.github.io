0851ecd084632733b82a37962711c62b98963da5
Moath <109114154+MoaathK@users.noreply.github.com>
2023-09-30 16:59:03 +0300

    uploading the whole website

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Portfolio />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+// src/components/Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Your Name</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
