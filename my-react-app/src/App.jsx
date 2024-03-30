8194cc9d843d8f2bd068d94266e50226e7622cff
Moath <109114154+MoaathK@users.noreply.github.com>
2024-03-31 00:38:03 +0300

    latest update

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,17 +2,28 @@ import Header from "./Header"
 import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
+import Button from "./button";
+import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 function App() {
   return(
     <>
     {/* <Header/>
     <Food/>
     <Footer/> */}
+    {/* <Card/>
     <Card/>
     <Card/>
     <Card/>
     <Card/>
-    <Card/>
+    <Button/>
+    <Student name="Spongbob" age={49} isStudent={true}/>
+    <Student name="Moath" age="22" isStudent={true}/>
+    <Student name="Azoz" age={21} isStudent={true}/>   
+    <Student name="Khalid" age={23} isStudent={false}/>   
+    <Student name="Lary"/>    */}
+    <UserGreeting isLoggedIn={true} username="Moath"/>
+    
 
     </>
 
